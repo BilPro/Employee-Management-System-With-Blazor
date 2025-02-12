3a9eb045907801697c8911bf39c75a608c312784
BilPro <bilal365@gmail.com>
2025-02-12 13:20:01 +0500

    Added UI & API project

---
+++ EmployeeManagementUI/Model/Models.cs
@@ -0,0 +1,38 @@
+﻿namespace EmployeeManagementUI.Model
+{
+    public class Employee
+    {
+        public int employeeID { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public int departmentID { get; set; }
+        public DateTime hireDate { get; set; }
+        public Department department { get; set; }
+    }
+
+    public class Department
+    {
+        public int departmentID { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+    }
+
+    public class AttendanceRecord
+    {
+        public int attendanceID { get; set; }
+        public int employeeID { get; set; }
+        public DateTime date { get; set; }
+        public string status { get; set; }
+    }
+
+    public class MissingAttendanceRequest
+    {
+        public int requestID { get; set; }
+        public int employeeID { get; set; }
+        public DateTime date { get; set; }
+        public string status { get; set; }
+        public int? approvedBy { get; set; }
+    }
+
+}
